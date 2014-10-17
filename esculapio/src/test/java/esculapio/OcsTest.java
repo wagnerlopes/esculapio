dc0da05e63af087f7f0a30fad442f417026e0d35
Wagner Lopes <wagner.luis.alopes@gmail.com>
2014-10-17 08:27:03 -0300

    Teste unitário de OCS.

---
+++ esculapio/src/test/java/esculapio/OcsTest.java
@@ -0,0 +1,22 @@
+package esculapio;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import br.com.wagnersoft.esculapio.model.Ocs;
+
+public class OcsTest extends TestCase {
+
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  @Test
+  public void testOcs() throws Exception {
+    Ocs ocs = new Ocs();
+    String c = ocs.getContato();
+    assertEquals("Teste", c);  
+  }
+  
+}
